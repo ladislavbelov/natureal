cee4aaa1d54242c55266cb1ac89044ecd0ecb1b4
ladislavbelov <klgbelov@gmail.com>
2023-04-01 11:52:46 +0300

    sections added

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,44 @@ window.addEventListener('load', function(){
 			roundFrameTop.style.top = window.scrollY / 10 + "%";
 		}
 	});
+
+	function isInViewport(element) {
+		const rect = element.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
+
+	// window.addEventListener('scroll', function(e){
+	// 	if (isInViewport(document.querySelector('.index__how'))) {
+	// 		console.log('first')
+	// 	}
+	// });
+
+	// const box = document.querySelector('.index__how');
+	// const rect = box.getBoundingClientRect();
+
+	// console.log(rect);
+	// function isInViewport(element) {
+	// 	var rect = element.getBoundingClientRect();
+	// 	var html = document.documentElement;
+	// 	return (
+	// 	  rect.top >= 0 &&
+	// 	  rect.left >= 0 &&
+	// 	  rect.bottom <= (window.innerHeight || html.clientHeight) &&
+	// 	  rect.right <= (window.innerWidth || html.clientWidth)
+	// 	);
+	//   }
+	  
+	//   $(window).scroll(function(){
+	// 	  $(".block").each(function(index,element){
+	// 		  if(isInViewport(element)){
+	// 			  //тут вешаешь класс на элемент меню
+	// 			  //только надо еще допилить случаи когда видно больше одного блока
+	// 		  }
+	// 	  })
+	//   })
 });
